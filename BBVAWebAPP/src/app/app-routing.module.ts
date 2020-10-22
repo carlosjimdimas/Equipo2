67f00da146a8cd3081bcbb057cedf4b4d3986493
ejcv.123 <ejcv.123@gmail.com>
2020-10-22 10:10:13 -0500

    Created the login component and added it to app-routing.module.ts

--- BBVAWebAPP/src/app/app-routing.module.ts
+++ BBVAWebAPP/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  // {path: 'register', component: ProductDetailsComponent},
+  // {path: 'movements', component: SearchComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
